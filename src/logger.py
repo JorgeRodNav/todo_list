fc02bbbbbbf4314a815b693d870801608152d11f
jorgedebian <jorgedebian@debian-jor.jorge.casa>
2025-07-12 19:38:50 +0200

    Added logging class. Lacks log ids

---
+++ src/logger.py
@@ -0,0 +1,30 @@
+"""!
+"""
+from pathlib import Path
+
+level_dict = {
+            0: "Error",
+            1: "Success",
+            2: "Warning"
+        }
+
+id_dict = {}
+
+class Log:
+    """!
+    Class for logging.
+    log_level can be: 0-Error, 1-Success, 2-Warning
+    log_id can be: TBD
+    log_msg: String with log message
+    """
+    def __init__(self, log_level:int, log_id:int, log_msg:str):
+        self.level = log_level
+        self.id = log_id
+        self.msg = log_msg
+
+    def record(self, log_file_path:Path):
+        try:
+            message = f"\n{level_dict[self.level]: <{id_dict[log_id]}>{self.msg}}"
+            with open(log_file_path, 'a') as file:
+                file.write(message)
+
